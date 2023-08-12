bdf96010d7ef33ba3a786523a76e178dce03d05e
shoaib <shoaibhossain24@gmail.com>
2023-08-12 07:41:22 +0600

    Type annotation of functions

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,16 @@
-console.log('hello, world');
+let greet: Function;
+
+greet = () => {
+    console.log('hello world');
+}
+
+const add = (a: number, b: number, c?: number | string ): void => {
+    console.log(a + b);
+    console.log(c);
+}
+
+add(5, 4);
+
+const minus = (a: number, b: number): number => {
+    return a + b;
+}
