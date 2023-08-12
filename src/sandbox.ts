fd629757f9acad490b51f1b7ffbc1cda545966a5
shoaib <shoaibhossain24@gmail.com>
2023-08-12 08:52:26 +0600

    function signature

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -23,3 +23,34 @@ const greetw = (user: objWithName) => {
     console.log(`${user.name} says hello`);
 }
 
+//function signature
+
+//example 1
+
+let greet1: (a : string, b: string) =>void;
+
+greet1 = (name: string, greeting: string) => {
+    console.log(`${name} says ${greeting}`);
+}
+
+//example 2
+
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) :number => {
+    if(action === 'add') {
+        return numOne + numTwo;
+    } else {
+        return numOne - numTwo;
+    }
+}
+
+//example 3
+
+let logDetails: (obj: {name: string, age: number}) => void;
+
+ logDetails = (workers: {name: string, age: number}) => {
+    console.log(`${workers.name} is ${workers.age} years old`); 
+};
+
+
