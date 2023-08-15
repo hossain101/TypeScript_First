522be5c12dfc033a4439c34efb9e5d61b0b5a809
shoaib <shoaibhossain24@gmail.com>
2023-08-15 06:34:31 +0600

    Add render method

--- public/app.js
+++ public/app.js
@@ -1,11 +1,16 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 const form = document.querySelector('form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
+    e.preventDefault();
     let doc;
     if (type.value === 'invoice') {
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
@@ -14,4 +19,5 @@ form.addEventListener('submit', (e) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
     console.log(doc);
+    list.render(doc, type.value, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import {Invoice} from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
@@ -10,10 +11,15 @@ const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 
 form.addEventListener('submit', (e: Event) => {
   
+    e.preventDefault();
+
     let doc: HasFormatter;
 
     if(type.value === 'invoice'){
@@ -21,10 +27,10 @@ form.addEventListener('submit', (e: Event) => {
     }else{
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     } 
-    
+
     console.log(doc);
-
-
+    
+    list.render(doc, type.value, 'end');
     });
 
     
